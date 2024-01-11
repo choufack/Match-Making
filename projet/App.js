6bdb5b4b52be4cf005204095145ae83a162633f0
linjacques <linjacques463@gmail.com>
2024-01-11 15:04:36 +0100

    revenir sur un commit précédent

--- projet/App.js
+++ projet/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import SignUp from './SignUp';
+import SignUp from './screens/SignUp';
 
-import Login from './Login'
+import Login from './screens/Login';
 // 
 
 const Stack = createStackNavigator();
