faefcc5f765441f906288e6e8e9598504e962736
amayas.bariz <amayas.bariz@gmail.com>
2024-01-11 15:16:12 +0100

    ajout listesport

--- projet/App.js
+++ projet/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import SignUp from './SignUp';
+import SignUp from './screens/SignUp';
 
-import Login from './Login'
+import Login from './screens/Login'
+
+// import List from './screens/Liste'
 // 
 
 const Stack = createStackNavigator();
@@ -15,8 +17,21 @@ const App = () => {
         <Stack.Screen name="SignUp" component={SignUp} />
         
         <Stack.Screen name="Login" component={Login} />
+
+        
         {/* <Stack.Screen name="UserModificationPage" component={UserModificationPage} /> */}
       </Stack.Navigator>
+
+      {/* <Stack.Navigator>
+      <Stack.Screen name="Liste" component={Liste} />
+
+      </Stack.Navigator> */}
+
+
+
+
+
+
     </NavigationContainer>
   );
 };
